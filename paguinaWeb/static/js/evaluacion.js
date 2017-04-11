9de2428d507215d7311084a88f5ca30b63aa0f75
jorenver <jorenver@espol.edu.ec>
2017-04-11 00:59:45 -0500

    calificacion de evaluacion

--- paguinaWeb/static/js/evaluacion.js
+++ paguinaWeb/static/js/evaluacion.js
@@ -1,13 +1,38 @@
 function inicializar(){
-	$("#btnCalificar").click(function(event){
-		var repestas=[]
+  $("#btnCalificar").click(function(event){
+    var tipo=event.target.dataset.tipo;
+    var id_evaluacion=event.target.dataset.id;
+    var peticion={  id_evaluacion:id_evaluacion,
+                    tipo:tipo,
+                    respuestas:[]};
 		for(var i=0;i<$(".opciones").length;i++){
 			var id_pregunta=$(".opciones")[i].dataset.id;
 			var opcion_seleccionada=$("input:radio[name="+id_pregunta+"]:checked").val() ? $("input:radio[name="+id_pregunta+"]:checked").val() : 0;
-			repestas.push({id_pregunta:id_pregunta,opcion_seleccionada:opcion_seleccionada});
+			peticion.respuestas.push({id_pregunta:id_pregunta,opcion_seleccionada:opcion_seleccionada});
 		}
-		alert(repestas);
-	});
+	 $.ajax({ 
+          url: '/calificar',
+          type: 'POST',
+          data: JSON.stringify(peticion),
+          contentType: 'application/json',
+          success: function(data){
+              if(!data.error){
+                swal("Su calificacion es:", data.calificacion+"/20","success").then(function(){
+                  window.location.href="/evaluaciones"
+                });
+              }else{
+                swal("Error!", "intente mas tarde", "error").then(function(){
+                  window.location.href="/"
+                });
+              }
+          },
+          error: function(){
+               swal("Error!", "intente mas tarde", "error").then(function(){
+                window.location.href="/"
+              });
+          }
+        })
+     });
 }
 
 window.addEventListener('load', inicializar, false);
