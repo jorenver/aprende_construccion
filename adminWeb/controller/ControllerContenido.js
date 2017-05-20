7f1ff8cf0aa1993ad90d1c53819ecced14cc7e1a
KevinOrtiz <kevanort@espol.edu.ec>
2017-05-19 23:13:23 -0500

    subir foto hecho

--- adminWeb/controller/ControllerContenido.js
+++ adminWeb/controller/ControllerContenido.js
@@ -1,5 +1,18 @@
 var db = require('../model/model');
 
+var multer = require('multer');
+
+var Storage = multer.diskStorage({
+    destination: function(request,file,callback){
+        callback(null,"static/images");
+    },
+    filename: function(request,file,callback){
+        callback(null, file.originalname);
+    }
+});
+
+var upload = multer({storage:Storage}).array("imgUploader",3);
+
 
 exports.loadContenido = function(request,response){
     db.contenido(request,response);
@@ -25,4 +38,15 @@ exports.eliminarPreguntaById = function(request,response){
 exports.guardarPregunta = function(request,response){
     db.guardarPregunta(request,response);
 
+}
+
+exports.uploadPicture = function(request,response){
+    upload(request,response,function(err){
+        if(err){
+            console.log("error algo paso");
+        }
+        else{
+            console.log("archivo subido exitosamente");
+        }
+    });
 }
